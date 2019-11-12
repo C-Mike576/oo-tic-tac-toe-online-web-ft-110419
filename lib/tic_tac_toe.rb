7ed20a417883c60238ff47ff27963c4e9ccffc82
Michael Calkins <m.calkins576@gmail.com>
2019-11-12 16:57:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,6 +76,10 @@ class TicTacToe
       if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
         return combo
       elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+        return combo
+      else
+        false
+      end
     end
   end  
   
