0cb1bf6d0e68efbfbbc61a7f8d3eef33aab193f8
Michael Calkins <m.calkins576@gmail.com>
2019-11-12 16:56:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,7 +73,9 @@ class TicTacToe
   
   def won?
     WIN_COMBINATIONS.each do |combo|
-      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && 
+      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+        return combo
+      elsif
     end
   end  
   
